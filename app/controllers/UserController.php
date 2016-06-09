7ef7d71f580fa0d4b6d76596d227538b3a8d54b1
Weifan Lin <wlin15@citymail.cuny.edu>
2016-06-09 19:21:40 -0400

    update UserController.php

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -7,6 +7,81 @@
  */
 class UserController extends BaseController {
     public function home() {
-        return 'home page';
+
+        $user = Auth::user();;
+        $posts = $user->post;
+        //return View::make('home')->with('posts',$posts);
+        return View::make('home',compact('posts'));
+    }
+
+    public function getLogin(){
+
+        return View::make('login');
+    }
+
+    public function postLogin(){
+        // validate user input
+        $rules = array(
+            'username'=>'required',
+            'password'=>'required|min:4'
+        );
+
+        $validator = Validator::make(Input::all(), $rules);
+
+        if ($validator->fails()){
+            //form invalid
+            return Redirect::to('login')
+                ->withErrors($validator)
+                ->withInput(Input::except('password'));
+        }
+
+        // attempt to login user
+        else {
+            $userdata = array(
+                'username'=>Input::get('username'),
+                'password'=>Input::get('password')
+            );
+
+            $remember = Input::has('remember') ? true:false;
+
+            if (Auth::attempt($userdata, $remember)){
+                //authentication successfull
+                return Redirect::route('home');
+            }
+
+            else {
+                //authentication failed
+                return Redirect::to('login')->with('message','Invalid username/password combination.')
+                    ->with('alert-class','alert-danger');
+            }
+        }
+    }
+
+    public function getRegister(){
+        return View::make('register');
+    }
+
+    public function postRegister(){
+        // validete user input
+        $validator = User::validate(Input::all());
+        if ($validator->passes()) {
+            // attempt to register the user
+            User::create(array(
+                'username'=>Input::get('username'),
+                'email'=>Input::get('email'),
+                'password'=>Input::get('password')
+            ));
+
+            return Redirect::route('login')->withMessage('You have successfully registered!');
+        }
+
+        return Redirect::route('register')->withErrors($validator);
+    }
+
+    public function logout() {
+        Auth::logout();
+        return Redirect::route('login')
+            ->withInput(Input::except('password'))
+            ->with('message','You have successfully logged out!');
     }
 }
