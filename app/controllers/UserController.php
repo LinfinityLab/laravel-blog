72d635877d49bde05813e9ab7f7b3ace468cccdb
Weifan Lin <wlin15@citymail.cuny.edu>
2016-06-09 17:41:55 -0400

    initial commit

---
+++ app/controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Linfinity
+ * Date: 6/9/16
+ * Time: 1:54 PM
+ */
+class UserController extends BaseController {
+    public function home() {
+        return 'home page';
+    }
+}
---
+++ app/views/hello.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+	<h1>{{ $var }}</h1>
+</body>
+</html>
