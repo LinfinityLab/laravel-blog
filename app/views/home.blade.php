b5d49dac3ed2b99c523a47651737c8d8de5e5290
Weifan Lin <wlin15@citymail.cuny.edu>
2016-06-10 14:24:16 -0400

    display no post if none

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,15 +3,20 @@
 @section('posts')
 
     <div class="container">
-        <h1>Hi {{ $username }}</h1>
+        <h1>Hi {{ $username }}!</h1>
         <hr>
         <div class="container">
-            <span STYLE="font-size: 18pt">My Posts</span> <span STYLE="float:right"><a href="/newpost">New</a></span>
-            <ul class="list-group">
-                @foreach ($posts as $post)
-                    <li class="list-group-item"> <a href="/post/?postid={{$post->id}}"> {{ $post->title }}</a> <span STYLE="float:right">{{$post->updated_at}}</span></li>
-                @endforeach
-            </ul>
+            <span STYLE="font-size: 18pt">My Posts</span> <span STYLE="float:right"><a href="/newpost">New</a></span><br><br>
+            @if (count($posts)>0)
+                <ul class="list-group">
+                    @foreach ($posts as $post)
+                        <li class="list-group-item"> <a href="/post/?postid={{$post->id}}"> {{ $post->title }}</a> <span STYLE="float:right">{{$post->updated_at}}</span></li>
+                    @endforeach
+                </ul>
+            @else
+                <p>You have no post.</p>
+            @endif
+
         </div>
     </div>
 
