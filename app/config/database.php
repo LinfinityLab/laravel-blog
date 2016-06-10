d5f99632720cc8456f65df072e167d14817f3b2b
Weifan Lin <wlin15@citymail.cuny.edu>
2016-06-10 18:57:53 -0400

    update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '127.0.0.1',
+			'host'      => 'localhost',
 			'database'  => 'blog',
-			'username'  => 'root',
-			'password'  => '',
+			'username'  => 'user',
+			'password'  => 'password',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
